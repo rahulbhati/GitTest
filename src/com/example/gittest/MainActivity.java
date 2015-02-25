b8b96b488fd5ffcbdf70e89f7154ce770ea36f05
rahulbhati <rahulbhati0042@gmail.com>
2015-02-25 10:46:17 +0530

    Branch create

--- src/com/example/gittest/MainActivity.java
+++ src/com/example/gittest/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		/**
+		 * Git Branch test 
+		 */
 	}
 
 
